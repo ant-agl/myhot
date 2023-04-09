0ce7cf514682f445865e684392155ad1a48828f2
ant-agl <aglanton31@gmail.com>
2023-04-09 17:31:43 +0300

    send user info

--- src/js/components/profile.js
+++ src/js/components/profile.js
@@ -1,125 +1,130 @@
-import Modal from './Modal';
-import ConfirmPassword from './ConfirmPassword.js';
+import Modal from "./Modal";
+import ConfirmPassword from "./ConfirmPassword.js";
 
-let confirmPass = new ConfirmPassword('.input-code-confirm', '.btn-confirm-password');
-let modalConfirm = new Modal('#modal-confirm-password', {
-    beforeOpen: () => {
-        confirmPass.clearAll();
-        confirmPass.focusFirst();
-        return true;
-    }
+let confirmPass = new ConfirmPassword(
+  ".input-code-confirm",
+  ".btn-confirm-password"
+);
+let modalConfirm = new Modal("#modal-confirm-password", {
+  beforeOpen: () => {
+    confirmPass.clearAll();
+    confirmPass.focusFirst();
+    return true;
+  },
 });
 confirmPass.afterSendSuccess = () => {
+  setTimeout(() => {
+    modalConfirm.close();
+  }, 750);
+};
+
+$(".input").prop("disabled", true);
+$(".change-data").on("click", function () {
+  $(this).toggleClass("active");
+  $(".input").prop("disabled", !$(this).hasClass("active"));
+  if ($(this).hasClass("active")) {
+    $('[name="change_password"]').val("").attr("type", "password");
+
+    $(".change-data-background").remove();
+    $("body").append('<div class="change-data-background"></div>');
+  } else {
+    $('[name="change_password"]').attr("type", "text");
+    let $bg = $(".change-data-background");
+    $bg.addClass("remove");
     setTimeout(() => {
-        modalConfirm.close();
-    }, 750);
-}
-
-$('.input').prop('disabled', true);
-$('.change-data').on('click', function() {
-    $(this).toggleClass('active');
-    $('.input').prop('disabled', !$(this).hasClass('active'));
-    if ($(this).hasClass('active')) {
-        $('[name="change_password"]')
-            .val('')
-            .attr('type', 'password');
-
-        $('.change-data-background').remove();
-        $('body').append('<div class="change-data-background"></div>');
-    } else {
-        $('[name="change_password"]')
-            .attr('type', 'text');
-        let $bg = $('.change-data-background');
-        $bg.addClass('remove');
-        setTimeout(() => {
-            $bg.remove();
-        }, 200);
-    }
+      $bg.remove();
+    }, 200);
+  }
 });
 
+$(".change-data").on("click", function () {
+  if ($(this).hasClass("active")) return;
+  let dfo = $('[name="dfo"]').data("last-value");
+  console.log(dfo);
 
+  let placeMessage = "";
+  switch (dfo) {
+    case "mail":
+      placeMessage = "на вашу почту";
+      break;
+    case "phone":
+      placeMessage = "на ваш телефон";
+      break;
+    case "app":
+      placeMessage = "в пуш уведомлении";
+      break;
+  }
+  $(".modal__desc span").text(placeMessage);
 
-$('.change-data').on('click', function() {
-    if ($(this).hasClass('active')) return;
-    let dfo = $('[name="dfo"]').data('last-value');
-    console.log(dfo);
+  let fio = [];
 
-    let placeMessage = '';
-    switch (dfo) {
-        case 'mail':
-            placeMessage = 'на вашу почту';
-            break;
-        case 'phone':
-            placeMessage = 'на ваш телефон';
-            break;
-        case 'app':
-            placeMessage = 'в пуш уведомлении';
-            break;
+  let data = {};
+  $(".input").each((i, item) => {
+    let name = $(item).attr("name");
+    let val = $(item).val().trim();
+    let lastVal = $(item).data("last-value");
+    if (name == "change_password" && val) {
+      data[name] = val;
+    } else if (name == "image" && val) {
+      var formData = new FormData();
+      formData.append(name, $(item)[0].files[0]);
+      data[name] = formData;
+    } else if (val && val != lastVal) {
+      data[name] = val;
+      $(item).data("last-value", val);
     }
-    $('.modal__desc span').text(placeMessage);
-
-    let fio = [];
-
-    let data = {};
-    $('.input').each((i, item) => {
-        let name = $(item).attr('name');
-        let val = $(item).val().trim();
-        let lastVal = $(item).data('last-value');
-        if (name == 'change_password') {
-            data[name] = val;
-        } else if (val && val != lastVal) {
-            data[name] = val;
-            $(item).data('last-value', val);
-        }
-        switch (name) {
-            case 'surname':
-            case 'name':
-            case 'second_name':
-                fio.push(val);
-                break;
-        }
-    });
-    $('.profile-menu__item a').first().text(fio.join(' '));
-    
-    if (data.change_password) {
-        $('[name="change_password"]')
-            .val('Был изменен сегодня')
-            .data('last-value', 'Был изменен сегодня');
-    } else {
-        let lastVal = $('[name="change_password"]').data('last-value');
-        $('[name="change_password"]').val(lastVal);
+    switch (name) {
+      case "surname":
+      case "name":
+      case "second_name":
+        fio.push(val);
+        break;
     }
+  });
+  $(".profile-menu__item a").first().text(fio.join(" "));
 
-    $('.msg').addClass('active');
-    setTimeout(() => {
-        $('.msg').removeClass('active');
-    }, 1500);
-    
-    if (dfo != 'no') {
-        if (data.dfo || data.number || data.change_password) {
-            modalConfirm.open();
-        }
+  if (data.change_password) {
+    $('[name="change_password"]')
+      .val("Был изменен сегодня")
+      .data("last-value", "Был изменен сегодня");
+  } else {
+    let lastVal = $('[name="change_password"]').data("last-value");
+    $('[name="change_password"]').val(lastVal);
+  }
+
+  $(".msg").addClass("active");
+  setTimeout(() => {
+    $(".msg").removeClass("active");
+  }, 1500);
+
+  if (dfo != "no") {
+    if (data.dfo || data.number || data.change_password) {
+      modalConfirm.open();
     }
-    if (data.email) {
-        $('.email-hint').removeClass('input-hint__not-active');
-    }
+  }
+  if (data.email) {
+    $(".email-hint").removeClass("input-hint__not-active");
+  }
 
-
-
-    console.log(data);
-    return;
-    $.ajax({
-        type: 'POST',
-        url: '',
-        data
-    });
+  console.log(data);
+  $.ajax({
+    type: "POST",
+    url: "http://wehotel.ru/handler/change_info.php",
+    data,
+    success: (data) => {
+      console.log(data);
+    },
+    error: (xhr) => {
+      console.log(xhr);
+    },
+  });
 });
 
-$('.input-avatar').on('change', function() {
-    let file = this.files[0];
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = function() {
-        $('.main__photo-img').attr('src', reader.result);
-    }
+$(".input-avatar").on("change", function () {
+  let file = this.files[0];
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = function () {
+    $(".main__photo-img").attr("src", reader.result);
+  };
 });
