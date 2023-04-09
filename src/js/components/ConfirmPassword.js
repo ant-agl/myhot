aeaa5e9103b1a7eeace42058cdd4412ca010097a
ant-agl <aglanton31@gmail.com>
2023-04-09 23:08:47 +0300

    send code

--- src/js/components/ConfirmPassword.js
+++ src/js/components/ConfirmPassword.js
@@ -1,5 +1,5 @@
 export default class ConfirmPassword {
-  constructor(selectorInputs, selectorBtn, settings={}) {
+  constructor(selectorInputs, selectorBtn, settings = {}) {
     this.$inputs = $(selectorInputs);
     this.$btn = $(selectorBtn);
 
@@ -7,17 +7,17 @@ export default class ConfirmPassword {
       this[key] = settings[key];
     }
 
-    this.$inputs.on('input keydown click', this.replaceFocus.bind(this));
-    this.$btn.on('click', this.sendCode.bind(this));
+    this.$inputs.on("input keydown click", this.replaceFocus.bind(this));
+    this.$btn.on("click", this.sendCode.bind(this));
   }
   replaceFocus(e) {
-    this.changeMessage('');
+    this.changeMessage("");
 
     this.$inputs.each((i, el) => {
       let val = $(el).val().trim();
       if (val.length > 1) {
         for (let j = 0; j < val.length; j++) {
-          this.$inputs.eq(i+j).val(val[j]);
+          this.$inputs.eq(i + j).val(val[j]);
         }
       }
     });
@@ -25,9 +25,12 @@ export default class ConfirmPassword {
     let key = e.key;
     let isFull = true;
     this.$inputs.each((i, el) => {
-      if ($(el).val().trim() == '') {
-        if ((key == 'Backspace' || key == 'Delete') && i > 0) {
-          this.$inputs.eq(i-1).val('').focus();
+      if ($(el).val().trim() == "") {
+        if ((key == "Backspace" || key == "Delete") && i > 0) {
+          this.$inputs
+            .eq(i - 1)
+            .val("")
+            .focus();
         } else {
           $(el).focus();
         }
@@ -35,42 +38,48 @@ export default class ConfirmPassword {
         return false;
       }
     });
-    if (isFull)
-      this.$btn.trigger('click');
+    if (isFull) this.$btn.trigger("click");
   }
   sendCode() {
-    let val = '';
+    let code = "";
     this.$inputs.each((i, el) => {
-      val += $(el).val().trim();
+      code += $(el).val().trim();
     });
 
-    let code = 1122;//test
-    if (val == code) {
-      this.changeMessage('Успешно!');
-      this.afterSendSuccess();
-    } else {
-      this.clearValue();
-      this.focusFirst();
-      this.changeMessage('Ошибка!', false);
-    }
-    return;
-    $.ajax();
+    $.ajax({
+      type: "POST",
+      url: "https://wehotel.ru/php/handler/code_check.php",
+      data: {
+        code,
+      },
+      success: (data) => {
+        console.log(data);
+        this.changeMessage("Успешно!");
+        this.afterSendSuccess();
+      },
+      error: (xhr) => {
+        console.log(xhr);
+        this.clearValue();
+        this.focusFirst();
+        this.changeMessage("Ошибка!", false);
+      },
+    });
   }
-  changeMessage(text='', success=true) {
-    $('.modal__message')
+  changeMessage(text = "", success = true) {
+    $(".modal__message")
       .text(text)
-      .removeClass('modal__message_success modal__message_error')
-      .addClass(success ? 'modal__message_success' : 'modal__message_error');
+      .removeClass("modal__message_success modal__message_error")
+      .addClass(success ? "modal__message_success" : "modal__message_error");
   }
   clearValue() {
-    this.$inputs.val('');
+    this.$inputs.val("");
   }
   clearAll() {
     this.clearValue();
     this.changeMessage();
   }
   focusFirst() {
-    setTimeout(() => { 
+    setTimeout(() => {
       this.$inputs.eq(0).focus();
     });
   }
