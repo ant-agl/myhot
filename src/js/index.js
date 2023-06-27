ca644a9581139b95550bd53509601c1bb66c67cc
ant-agl <aglanton31@gmail.com>
2023-06-27 19:32:46 +0300

    test

--- src/js/index.js
+++ src/js/index.js
@@ -84,6 +84,8 @@ $("body").on("click", ".send_button", function (e) {
   });
 });
 //efwef
+//efwef
+//efwef
 $("body").on("click", ".btn-go-sup", function (e) {
   e.preventDefault();
   $(".chats__item.support-chat").trigger("click");
