db2726ca605ede51c767a966876093485ddfa519
ant-agl <aglanton31@gmail.com>
2023-06-29 19:04:35 +0300

    update

--- src/js/lk.js
+++ src/js/lk.js
@@ -205,7 +205,7 @@ let validationForm = new Validation(".form_send", {
   isOutputErrors: false,
 });
 
-$("body").on("click", ".send_button_support", function (e) {
+$(".form_send").on("submit", function (e) {
   e.preventDefault();
 
   if (!validationForm.validate()) return;
