e38d994d0c440c3eece2b4aa4f65cd14d923421f
ant-agl <aglanton31@gmail.com>
2023-07-08 23:49:09 +0600

    update

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -132,7 +132,7 @@ $(".form-search").on("submit", function (e) {
 });
 
 $("body").keyup(function (e) {
-  if (e.keyCode === 13 && $(".find-hint__item.select").length == 0) {
+  if (e.keyCode === 13 && $(".find-hint.active").length == 0) {
     if ($(".modal_open").length == 0) {
       $(".search__button").eq(0).trigger("click");
     }
