c8f41570f47af8b2f056994323f66c16e6b3cfed
ant-agl <aglanton31@gmail.com>
2023-07-17 12:07:15 +0300

    update

--- src/js/components/profile.js
+++ src/js/components/profile.js
@@ -250,7 +250,7 @@ $('[name="image"]').on("change", function () {
 
     cropper = new Cropper($("#crop-avatar")[0], {
       aspectRatio: 1 / 1,
-      viewMode: 1,
+      viewMode: 3,
     });
 
     modalAvatar.open();
